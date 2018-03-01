ae14faed23085291986c362c0ff3cdde9350b064
buckbeak <buckbeak@protonmail.com>
2018-02-28 19:30:07 -0500

    Listen and Serve on tcp4:8088.

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 )
 
 const (
 	// the listening port
-	port = ":80"
+	port = ":8088"
 	//port = ":9155"
 )
 
 func main() {
 	service := NewService()
 	log.Println("hxwebapi started on", port)
-	log.Fatal(http.ListenAndServe(port, service.Router))
+	l, err := net.Listen("tcp4", port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.Serve(l, service.Router))
 }
